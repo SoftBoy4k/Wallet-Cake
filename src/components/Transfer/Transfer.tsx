278d2073563c8499b357873e80e6bca4c6ade112
Vladislav Pimoshenko <vladislavpimoshenko@gmail.com>
2024-04-16 23:40:02 +0300

    created transfer operations

--- src/components/Transfer/Transfer.tsx
+++ src/components/Transfer/Transfer.tsx
@@ -1,18 +1,21 @@
 import './Transfer.css'
-import moreIcon from '../../icons/more.png'
 import { ShortCard } from './ShortCard/ShortCard'
 import { ShortNoCard } from './ShortNoCard/ShortNoCard'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { RootState } from '../../redux/store'
 import { useState } from 'react'
 import { SelectionIcons } from './SelectionIcons/SelectionIcons'
+import { editAccounts } from '../../redux/Slices/accountsSlice'
 
 export const Transfer = () => {
 
+  const dispatch = useDispatch();
   const accounts = useSelector((state: RootState) => state.accounts.accounts);
   const [selectedShortCard, setSelectedShortCard] = useState<number>(0);
   const [isSelectedIcon, setIsSelectedIcon] = useState<boolean>(false);
   const [selectedIcon, setSelectedIcon] = useState<string | undefined>();
+  const [transferOperations, setTransferOperations] = useState<0 | 1>(0); // 0 - Expenses, 1 - Income
+  const [transferAmount, setTransferAmount] = useState<number>(0);
 
   const shortCardSelectHandler = (i:number) => {
     setSelectedShortCard(i)
@@ -21,12 +24,33 @@ export const Transfer = () => {
   const selectionIconsHandler = () => {
     setIsSelectedIcon(!isSelectedIcon);
   }
+
+  const sendHandler = ():void => {
+    const currentAccounts = {
+      id: selectedShortCard,
+      name: accounts[selectedShortCard].name,
+      background: accounts[selectedShortCard].background,
+      amount: transferOperations === 0 ? accounts[selectedShortCard].amount - transferAmount : accounts[selectedShortCard].amount + transferAmount
+    }
+    dispatch(editAccounts(currentAccounts))
+  }
  
   return (
     <div className="transfer">
         <div className='block__header'>
           <h4>Transfer</h4>
-          <img className='transfer__icon' src={moreIcon} alt="more" />
+          <div className='transfer__operations'>
+            <p 
+            className={transferOperations === 0 ? 'transfer__operation--active' : undefined}
+            onClick={() => setTransferOperations(0)}>
+              Expenses
+            </p>
+            <p 
+            className={transferOperations === 1 ? 'transfer__operation--active' : undefined} 
+            onClick={() => setTransferOperations(1)}>
+              Income
+            </p>
+          </div>
         </div>
         <div className='transfer__cards'>
           {[0, 1, 2].map(i => accounts[i] ? <ShortCard account={accounts[i]} key={i} isSelected={selectedShortCard === i} shortCardSelectHandler={shortCardSelectHandler}/> : <ShortNoCard key={i}/>)}
@@ -52,10 +76,14 @@ export const Transfer = () => {
             <div>
               <p className='transfer__text'>Amount ($)</p>
               <div>
-                <input className='transfer__input' type="text" />
+                <input 
+                  className='transfer__input' 
+                  type="text" 
+                  value={transferAmount} 
+                  onChange={e => setTransferAmount(Number(e.target.value))}/>
               </div>
             </div>
-            <button>Send</button>
+            <button onClick={sendHandler}>Send</button>
           </div>
         </div>
     </div>
