399acd84156b5af98cb21d36afe3ec353dec571c
Vladislav Pimoshenko <vladislavpimoshenko@gmail.com>
2024-05-16 21:14:06 +0300

    uninstall react-routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,6 @@
 import './App.css'
 import HomePage from './Pages/HomePage/HomePage'
-import { Header } from './components/Header/Header'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import SingInPage from './Pages/SingInPage/SingInPage.tsx'
-import ErrorPage from './Pages/ErrorPage/ErrorPage.tsx'
-
-
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <HomePage/>,
-    errorElement: <ErrorPage/>
-  },
-  {
-    path: 'sing-in',
-    element: <SingInPage/>
-  }
-]);
+import { Header } from './components/Header/Header.tsx'
 
 
 function App() {
@@ -25,7 +8,7 @@ function App() {
   return (
     <div className='app'>
       <Header/>
-      <RouterProvider router={router}/>
+      <HomePage/>
     </div>
   )
 }
